a3a37b780adad34982cff9c129d7854e5994979e
pattyjubu <one.two.jungo@gmail.com>
2014-11-03 20:45:52 +0700

    crete data in index.js

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ var express = require('express')
 var app = express()
 
 app.use(express.static(__dirname + '/public'))
+
+app.get('/api/book', function(req, res){
+	var books = [
+		{title: 'Angular', price: 800},
+		{title: 'Nodejs', price: 500},
+		{title: 'CSS', price: 200},
+	];
+	res.send(books);
+})
  
 var server = app.listen(3000, function () {
 
