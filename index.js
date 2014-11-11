523d8470ac2d6080825788b2df8868ed969956f7
pattyjubu <one.two.jungo@gmail.com>
2014-11-11 23:56:18 +0700

    add mongodb

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
 var express = require('express')
+var mongojs = require('mongojs')
+var bodyParser = require('body-parser')
 var app = express()
+var db = mongojs('my_server', ['book']);
 
 app.use(express.static(__dirname + '/public'))
+app.use(bodyParser.json())
 
 app.get('/api/book', function(req, res){
-	var books = [
-		{title: 'Angular', price: 800},
-		{title: 'Nodejs', price: 500},
-		{title: 'CSS', price: 200},
-		{title: 'Javascript', price: 400},
-	];
-	res.send(books);
+	db.book.find({}, function(err,docs){
+		res.send(docs);
+	});
+
 })
- 
+
+/*app.post('/api/book', function(req, res){
+	db.book/insert(req.body,function(err,docs){
+		res.send(docs);
+	});
+});*/
+
 var server = app.listen(3000, function () {
-
-  var host = server.address().address
-  var port = server.address().port
-
-  console.log('Example app listening at http://%s:%s', host, port)
-
+	var host = server.address().address
+  	var port = server.address().port
+  	console.log('Example app listening at http://%s:%s', host, port)
 })
